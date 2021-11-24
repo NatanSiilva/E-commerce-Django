9af3991aa390fd378f2ba345168d84a568fcd66f
Natanael <natanaelsilva.progjs@gmail.com>
2021-11-23 21:53:40 -0300

    feat: module validate

--- profile_user/models.py
+++ profile_user/models.py
@@ -1,3 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import User
+# from utils.data import states
 
-# Create your models here.
+
+class ProfileUser(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, verbose_name='Usuário')
+    age = models.PositiveBigIntegerField(verbose_name='Idade')
+    birth_date = models.DateField(verbose_name='Data de Nascimento')
+    cpf = models.CharField(max_length=11, verbose_name='CPF')
+    address = models.CharField(max_length=50, verbose_name='Endereço')
+    number = models.CharField(max_length=5, verbose_name='Número')
+    complement = models.CharField(max_length=30, verbose_name='Complemento')
+    neighborhood = models.CharField(max_length=30, verbose_name='Bairro')
+    cep = models.CharField(max_length=8, verbose_name='CEP')
+    city = models.CharField(max_length=30, verbose_name='Cidade')
+    state = models.CharField(
+        max_length=2,
+        default='SP',
+        choices=(
+            ('AC', 'Acre'),
+            ('AL', 'Alagoas'),
+            ('AP', 'Amapá'),
+            ('AM', 'Amazonas'),
+            ('BA', 'Bahia'),
+            ('CE', 'Ceará'),
+            ('DF', 'Distrito Federal'),
+            ('ES', 'Espírito Santo'),
+            ('GO', 'Goiás'),
+            ('MA', 'Maranhão'),
+            ('MT', 'Mato Grosso'),
+            ('MS', 'Mato Grosso do Sul'),
+            ('MG', 'Minas Gerais'),
+            ('PA', 'Pará'),
+            ('PB', 'Paraíba'),
+            ('PR', 'Paraná'),
+            ('PE', 'Pernambuco'),
+            ('PI', 'Piauí'),
+            ('RJ', 'Rio de Janeiro'),
+            ('RN', 'Rio Grande do Norte'),
+            ('RS', 'Rio Grande do Sul'),
+            ('RO', 'Rondônia'),
+            ('RR', 'Roraima'),
+            ('SC', 'Santa Catarina'),
+            ('SP', 'São Paulo'),
+            ('SE', 'Sergipe'),
+            ('TO', 'Tocantins'),
+        ),
+        verbose_name='Estado'
+    )
+
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
+
+    def clean(self):
+        print(self.age)
+
+    class Meta:
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfis'
